e4cbfe8d4830b33eb5c38fd4fd4425705cb915a4
rowaidsayyed <rowaidsayyed@gmail.com>
2020-06-29 21:06:34 +0300

    add react yml

---
+++ build/precache-manifest.9e59747c9261c324ac1d66c9faf0e813.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "071799fe7354918eba5f1db83017b0b5",
+    "url": "/resty/index.html"
+  },
+  {
+    "revision": "ef2b65adbeb4a61fd837",
+    "url": "/resty/static/css/main.4c5e5156.chunk.css"
+  },
+  {
+    "revision": "64909891a31834ed1312",
+    "url": "/resty/static/js/2.25b57a3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resty/static/js/2.25b57a3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef2b65adbeb4a61fd837",
+    "url": "/resty/static/js/main.eb241dfd.chunk.js"
+  },
+  {
+    "revision": "a82df10f567bbeaa6ec2",
+    "url": "/resty/static/js/runtime-main.ea9bec24.js"
+  }
+]);
