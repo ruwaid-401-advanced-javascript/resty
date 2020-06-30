7d10e720d5628143d719890315b56008e4993be4
rowaidsayyed <rowaidsayyed@gmail.com>
2020-07-01 00:11:37 +0300

    fix tests

--- src/__tests__/app.test.js
+++ src/__tests__/app.test.js
@@ -4,7 +4,8 @@ import renderer from 'react-test-renderer';
 
 import Form from '../form';
 import Header from '../header';
-import Footer from '../footer'
+import Footer from '../footer';
+import Results from '../results';
 
 describe('<App/>', () => {
   it('is alive at application start', () => {
@@ -19,7 +20,7 @@ describe('<App/>', () => {
 
   it('is alive at application start', () => {
     let app = shallow(<Form />);
-    expect(app.find('span').exists()).toBeTruthy();
+    expect(app.find('button').exists()).toBeTruthy();
   });
 
   it('renders correctly', () => {
@@ -29,6 +30,11 @@ describe('<App/>', () => {
 
   it('renders correctly', () => {
     const tree = renderer.create(<Header />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders correctly', () => {
+    const tree = renderer.create(<Results />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
