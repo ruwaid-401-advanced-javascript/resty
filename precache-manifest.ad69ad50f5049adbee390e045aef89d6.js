ad922b26496eeb3e8c9280de98f2f864bed06a51
rowaidsayyed <rowaidsayyed@gmail.com>
2020-06-30 03:22:00 +0300

    Updates

---
+++ precache-manifest.ad69ad50f5049adbee390e045aef89d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edbd56526d6a22030b79a61abfb169b6",
+    "url": "/resty/index.html"
+  },
+  {
+    "revision": "da696d6c9df0bba4a38f",
+    "url": "/resty/static/css/main.4c5e5156.chunk.css"
+  },
+  {
+    "revision": "64909891a31834ed1312",
+    "url": "/resty/static/js/2.25b57a3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resty/static/js/2.25b57a3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da696d6c9df0bba4a38f",
+    "url": "/resty/static/js/main.af0a40bc.chunk.js"
+  },
+  {
+    "revision": "a82df10f567bbeaa6ec2",
+    "url": "/resty/static/js/runtime-main.ea9bec24.js"
+  }
+]);
