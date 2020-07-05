40497562027b78add8f7898ac35da1743d494ee5
rowaidsayyed <runner@fv-az67.1iv5y5haxxxefcepptnum51jze.gx.internal.cloudapp.net>
2020-07-05 10:37:34 +0000

    Updates

---
+++ precache-manifest.cbb5dea5531cb70b4e0e8522ed8d8544.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfcd0d62bdf725334c59cc2df18b6863",
+    "url": "/resty/index.html"
+  },
+  {
+    "revision": "ba481b2105987249091b",
+    "url": "/resty/static/css/main.3118e304.chunk.css"
+  },
+  {
+    "revision": "6b6e30d2b6a926438745",
+    "url": "/resty/static/js/2.7c9aef69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resty/static/js/2.7c9aef69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba481b2105987249091b",
+    "url": "/resty/static/js/main.2ceb884c.chunk.js"
+  },
+  {
+    "revision": "a82df10f567bbeaa6ec2",
+    "url": "/resty/static/js/runtime-main.ea9bec24.js"
+  }
+]);
